172f73e03e85fea1c9be066fd4c0965ab4667cb6
Josef Vacek <vacekj@outlook.com>
2023-03-23 00:10:34 +0100

    parse events

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,56 @@
-use std::error::Error;
 use ethers::{
-    core::types::{Address, Filter, H160, H256, U256},
+    core::types::{Address, Filter},
     providers::{Http, Middleware, Provider},
 };
 use eyre::Result;
 use std::sync::Arc;
+use ethers::abi::{decode, Uint};
+use ethers::abi::ethabi::Bytes;
+use ethers::abi::ParamType::{Address as ParamAddress, Uint as AbiUint};
+use ethers::types::H256;
 
 const HTTP_URL: &str = "https://rpc.flashbots.net";
-const V3FACTORY_ADDRESS: &str = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
-const DAI_ADDRESS: &str = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-const USDC_ADDRESS: &str = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-const USDT_ADDRESS: &str = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+const ROUND_ADDRESS: &str = "0x746b951FA10a89d6cbe70d4EE23531f907B58Bc0";
+
+#[derive(Debug)]
+struct Vote {
+    token: Address,
+    amount: Uint,
+    voter: Address,
+    grant_address: Address,
+    project_id: Bytes,
+    round_address: Address,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let provider = Provider::<Http>::try_from(HTTP_URL)?;
     let client = Arc::new(provider);
-    let token_topics = vec![
-        H256::from(USDC_ADDRESS.parse::<H160>()?),
-        H256::from(USDT_ADDRESS.parse::<H160>()?),
-        H256::from(DAI_ADDRESS.parse::<H160>()?),
-    ];
     let filter = Filter::new()
-        .address(V3FACTORY_ADDRESS.parse::<Address>()?)
-        .event("PoolCreated(address,address,uint24,int24,address)")
-        .topic1(token_topics.to_vec())
-        .topic2(token_topics.to_vec())
-        .from_block(0);
+        .address(ROUND_ADDRESS.parse::<Address>()?)
+        .event("Voted(address,uint256,address,address,bytes32,address)")
+        .from_block(16530241);
     let logs = client.get_logs(&filter).await?;
-    println!("{} pools found!", logs.iter().len());
+    println!("{} votes found!", logs.iter().len());
+
+    let mut votes = vec![];
     for log in logs.iter() {
-        let token0 = Address::from(log.topics[1]);
-        let token1 = Address::from(log.topics[2]);
-        let fee_tier = U256::from_big_endian(&log.topics[3].as_bytes()[29..32]);
-        let tick_spacing = U256::from_big_endian(&log.data[29..32]);
-        let pool = Address::from(&log.data[44..64].try_into()?);
-        println!(
-            "pool = {pool}, token0 = {token0}, token1 = {token1}, fee = {fee_tier}, spacing = {tick_spacing}"
-        );
+        let types = vec![ParamAddress, AbiUint(256), ParamAddress];
+        let decoded = decode(&types, &log.data);
+        if let Ok(decoded) = decoded {
+            let vote = Vote {
+                token: decoded[0].clone().into_address().unwrap_or_default(),
+                amount: decoded[1].clone().into_uint().unwrap_or_default(),
+                voter: Default::default(),
+                grant_address: decoded[2].clone().into_address().unwrap_or_default(),
+                project_id: vec![],
+                round_address: Default::default(),
+            };
+            votes.push(vote);
+        };
     }
+
+    dbg!(votes);
+
     Ok(())
 }
